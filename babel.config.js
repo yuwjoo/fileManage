8e48a63763697753da7e92b767ae024dcd70051f
YH <1191759350@qq.com>
2023-07-08 12:00:26 +0800

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ["@babel/preset-env"],
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk",
+      },
+    ],
+  ],
 };
--- src/library/useElementUI.js
+++ src/library/useElementUI.js
@@ -1,5 +1,6 @@
-import elementUI from "element-ui";
+import { Button, Select } from "element-ui";
 
 export function useElementUI(Vue) {
-    
+  Vue.component(Button.name, Button);
+  Vue.component(Select.name, Select);
 }
